bc5898883994e8212fab5a7e0daba39643b82c5b
s-oyama-018 <shogoyama0721@gmail.com>
2022-07-05 17:49:56 +0900

    1 file modified, 1 added

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 
 import { products } from '../products';
+import { Product } from "../shared/product.model"
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css'],
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  products:Product[] = products;
 
   share() {
     window.alert('The product has been shared!');
   }
 }
+
 
 /*
 Copyright Google LLC. All Rights Reserved.
