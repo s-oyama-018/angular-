5b5ab61f8b8199e317c6de359e87e2e59bc355be
s-oyama-018 <shogoyama0721@gmail.com>
2022-07-05 15:30:10 +0900

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,19 +13,13 @@ import { ProductListComponent } from './product-list/product-list.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-    ])
+      { path: 'topbar', component: TopBarComponent },
+    ]),
   ],
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    ProductListComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
+  declarations: [AppComponent, TopBarComponent, ProductListComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
